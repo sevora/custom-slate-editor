6e5ee0226fda5f4dedb3d18dd61da5c99cca3b74
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2023-10-03 12:49:50 +0800

    Improve Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 
 import react from '@vitejs/plugin-react-swc';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import svgr from 'vite-plugin-svgr';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
-// https://vitejs.dev/config/
+/**
+ * This is the vite.js configuration which would 
+ * influence how the vite toolkit works.
+ */
 export default defineConfig({
   plugins: [
     react(),
@@ -14,17 +19,36 @@ export default defineConfig({
     svgr(),
     dts({ include: [ 'editor' ]})
   ],
+
   build: {
+    copyPublicDir: true,
     lib: {
       entry: resolve(__dirname, 'editor/index.tsx'),
       formats: ['es'],
     },
+
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      external: [ 'react', 'react/jsx-runtime' ],
+
+      /**
+       * These turns all the .ts or .tsx files into
+       * entrypoints.
+       */
+      input: Object.fromEntries(
+        glob.sync('editor/**/*.{ts,tsx}').map(file => [
+          relative('editor', file.slice(0, file.length - extname(file).length) ),
+          fileURLToPath(new URL(file, import.meta.url))
+       ])
+      ),
+
+      /**
+       * We want to make sure the names of the 
+       * output files are pretty.
+       */
       output: {
-        entryFileNames: 'index.js'
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js'
       }
-    },
-    copyPublicDir: false
-  },
-})
+    }
+  }
+});
